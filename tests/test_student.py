d33ef31252723f70a4348f1443389221cb5e586e
sheldoncooper321 <20B-048-CS@students.uit.edu>
2025-01-05 22:49:06 +0500

    Create test_student.py

---
+++ tests/test_student.py
@@ -0,0 +1,8 @@
+# tests/test_student.py
+import pytest
+
+def test_add_student():
+    assert True  # Replace with real test logic
+
+def test_delete_student():
+    assert True  # Replace with real test logic
